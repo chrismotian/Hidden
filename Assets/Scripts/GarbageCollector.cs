1a49e759668f85750de5abd59a0c8d53cc277b6c
chrismotian <chrisjberlin@googlemail.com>
2021-04-10 05:20:20 +0800

    add particle when click

---
+++ Assets/Scripts/GarbageCollector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageCollector : MonoBehaviour
+{
+    [SerializeField] float cooldown = 3;
+    float timeAlive = 0;
+    void Update()
+    {
+        timeAlive = timeAlive + Time.deltaTime;
+        if (timeAlive > cooldown)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public float cooldown = 3;
+    float timeAtLastSpawn = 0;
+    [SerializeField] GameObject[] obj = null;
+    void Update()
+    {
+        if ((Input.GetMouseButtonDown(0)) && (Time.time - timeAtLastSpawn) > cooldown)
+        {
+            timeAtLastSpawn = Time.time;
+            GameObject instance = (GameObject)Instantiate(obj[Random.Range(0, obj.GetLength(0))], Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+            instance.transform.parent = this.transform;
+        }else if(Input.touchCount > 0 && (Time.time - timeAtLastSpawn) > cooldown)
+        {
+            timeAtLastSpawn = Time.time;
+            GameObject instance = (GameObject)Instantiate(obj[Random.Range(0, obj.GetLength(0))], Camera.main.ScreenToWorldPoint(Input.touches[0].position), Quaternion.identity);
+            instance.transform.parent = this.transform;
+        }
+    }
+}
